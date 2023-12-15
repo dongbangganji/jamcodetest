9d319a13be1dbabf4f9781b6182bd689f293a2f4
carry <lsy1@torder.co.kr>
2023-12-15 19:38:52 +0900

    feat : fitness 도메인 기능 구현

---
+++ laravel/config/fitness.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'Crossfit' => ['enough_money', 'strong_will'],
+    'Cardio Exercise' => ['strong_will'],
+    'Strength' => ['strong_will', 'enough_time'],
+    'Spinning' => ['enough_money']
+];
